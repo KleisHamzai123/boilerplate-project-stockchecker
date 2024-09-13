a5064144d7b04cb7e0bff0b4f59c148effed3e86
Kleis Hamzai <kleishamzai123@gmail.com>
2024-09-13 15:05:51 +0000

    oki

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,42 @@
 'use strict';
+const StockModel = require("../models").Stock;
+const fetch = require("node-fetch"); 
+
+
+
+
+
+async function getStock(stock){
+  const response = await fetch('https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote');
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice};
+}
+
+async function saveStock(stock, like, ip){
+
+  let saved = {};
+  const foundStock = await findStock(stock);
+  if (!foundStock) {
+    const createsaved = await createStock(stock, like, ip);
+    saved = createsaved;
+    return saved;
+  }else{
+    if (like && foundStock.likes.indexOf(ip)=== -1){foundStock.likes.push(ip);} saved = await foundStock.save();
+    return saved;
+  }
+  
+}
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(function async (req, res){
+      const { stock, like } = req.query;
+      const { symbol, latestPrice } = await getStock(stock);
+      if (!symbol){
+        res.json ({ stockData: { likes: like ? 1: 0 }});
+        return;
+      }
     });
     
 };
