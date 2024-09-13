da26210c9791751c0e4b8f1f6c6008b198941505
Kleis Hamzai <kleishamzai123@gmail.com>
2024-09-13 13:57:18 +0000

    asa

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const cors        = require('cors');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+require(./db-connection");
 
-require("./db-connectio.js");
+
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
