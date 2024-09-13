24b2d9b9b52ddcb985951393a2c40d266f4a8709
Kleis Hamzai <kleishamzai123@gmail.com>
2024-09-13 21:46:04 +0000

    j

--- routes/db-connection.js
+++ routes/db-connection.js
@@ -1,3 +1,7 @@
-const mongoose = require("mongoose");
-const db = mongoose.connect(process.env.DB, {useUnifiedTopology: true, useNewUrlParser: true, });
-module.exports = db;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;
