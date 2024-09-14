0e49956969fd9cfbd4c4fb9571605e71c5aeedc5
Kleis Hamzai <kleishamzai123@gmail.com>
2024-09-14 00:15:58 +0000

    pppp

--- routes/db-connection.js
+++ routes/db-connection.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
-// Replace `process.env.DB` with your connection string directly for testing
 const dbURI = 'mongodb+srv://kleishamzai123:Pallati1290!@cluster0.nzyzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-const db = mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-module.exports = db;
+mongoose.connect(dbURI)
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.error('Database connection error:', err));
