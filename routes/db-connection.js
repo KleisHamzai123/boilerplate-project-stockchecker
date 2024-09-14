bed8374d9d73ffefbbabf071dfbaa197a7aba23d
Kleis Hamzai <kleishamzai123@gmail.com>
2024-09-14 00:18:37 +0000

    re

--- routes/db-connection.js
+++ routes/db-connection.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const dbURI = 'mongodb+srv://kleishamzai123:Pallati1290!@cluster0.nzyzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(dbURI)
+mongoose.connect(dbURI, {
+  connectTimeoutMS: 50000, // 30 seconds
+  socketTimeoutMS: 50000  // 30 seconds
+})
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.error('Database connection error:', err));
